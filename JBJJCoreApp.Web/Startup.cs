4106d89f893cd4eefbb049f315378b979a7983c2
joeipe <joe.ipe@hotmail.com>
2019-05-19 19:53:11 +1000

    Adding swagger

--- JBJJCoreApp.Web/Startup.cs
+++ JBJJCoreApp.Web/Startup.cs
@@ -17,6 +17,7 @@ using Schedule.Data;
 using Schedule.Data.Services;
 using Student.Data;
 using Student.Data.Services;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace JBJJCoreApp.Web
 {
@@ -57,6 +58,12 @@ namespace JBJJCoreApp.Web
             services.AddScoped<DayAtDojoData>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "JBJJCoreApp API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +80,16 @@ namespace JBJJCoreApp.Web
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JBJJCoreApp API V1");
+            });
         }
     }
 }
